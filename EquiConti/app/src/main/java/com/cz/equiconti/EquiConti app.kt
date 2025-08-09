7988fce1b7d5ead3c395ccd1efb4113d8b0dcd09
fbk76 <fadilalb@yahoo.it>
2025-08-09 18:26:31 +0200

    Create EquiConti app.kt

---
+++ EquiConti/app/src/main/java/com/cz/equiconti/EquiConti app.kt
@@ -0,0 +1,15 @@
+package com.cz.equiconti
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EquiContiApp() {
+    MaterialTheme {
+        Surface {
+            Text("EquiConti è vivo 🐴")
+        }
+    }
+}
