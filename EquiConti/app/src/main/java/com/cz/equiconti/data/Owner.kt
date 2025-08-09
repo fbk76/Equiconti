784589d2c3bc077bd5869478501fee1737203e73
fbk76 <fadilalb@yahoo.it>
2025-08-10 00:22:28 +0200

    Create Owner.kt

---
+++ EquiConti/app/src/main/java/com/cz/equiconti/data/Owner.kt
@@ -0,0 +1,13 @@
+package com.cz.equiconti.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "owners")
+data class Owner(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val firstName: String,
+    val lastName: String,
+    val phone: String? = null,
+    val email: String? = null
+)
