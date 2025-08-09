e0ed2d624a92f86a3c23a7b54409c1a0796c7a4d
fbk76 <fadilalb@yahoo.it>
2025-08-09 09:12:07 +0200

    Update build.gradle.kts

--- EquiConti/build.gradle.kts
+++ EquiConti/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.kapt") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
-    // Hilt (plugin di progetto)
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
