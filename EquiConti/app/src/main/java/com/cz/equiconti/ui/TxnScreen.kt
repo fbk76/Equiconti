d3bea349dd7676d10b993cbe3a699f2c5b791fcb
fbk76 <fadilalb@yahoo.it>
2025-08-11 00:15:37 +0200

    Update TxnScreen.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/ui/TxnScreen.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/ui/TxnScreen.kt
@@ -1,5 +1,5 @@
 package com.cz.equiconti.ui
-
+import com.cz.equiconti.data.Txn
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
