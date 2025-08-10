866b307c94ca4d0ebd771d079c6f4da0332e2d38
fbk76 <fadilalb@yahoo.it>
2025-08-10 13:26:04 +0200

    Update HorseDao.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/data/HorseDao.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/data/HorseDao.kt
@@ -15,6 +15,11 @@ interface HorseDao {
     @Delete
     suspend fun delete(horse: Horse)
 
+    // Flow osservabile (usato da Repo)
+    @Query("SELECT * FROM Horse WHERE ownerId = :ownerId ORDER BY name")
+    fun observeByOwner(ownerId: Long): Flow<List<Horse>>
+
+    // Se vuoi anche una funzione con lo stesso SQL ma nome diverso:
     @Query("SELECT * FROM Horse WHERE ownerId = :ownerId ORDER BY name")
     fun listByOwner(ownerId: Long): Flow<List<Horse>>
 
