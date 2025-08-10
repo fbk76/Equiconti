bd9f3f1502be3457b8c5c42d69e45a480f3a937f
fbk76 <fadilalb@yahoo.it>
2025-08-11 00:26:31 +0200

    Update TxnScreen.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/ui/TxnScreen.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/ui/TxnScreen.kt
@@ -26,7 +26,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.cz.equiconti.data.Txn
+
 import com.cz.equiconti.util.formatCurrency
 import com.cz.equiconti.vm.TxnVm
 import java.time.LocalDate
