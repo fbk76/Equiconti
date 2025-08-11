7cc83d70842efb7aa3618f2c21d5b694e17e6cff
fbk76 <fadilalb@yahoo.it>
2025-08-12 01:28:48 +0200

    Update MainActivity.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/MainActivity.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/MainActivity.kt
@@ -3,24 +3,29 @@ package com.cz.equiconti
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
-import com.cz.equiconti.ui.NavGraph
 import dagger.hilt.android.AndroidEntryPoint
+import com.cz.equiconti.ui.nav.AppNavGraph
+import com.cz.equiconti.ui.theme.EquiContiTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Se hai un tema tuo, rimpiazza con quello. Altrimenti MaterialTheme va benissimo.
-            MaterialTheme {
-                Surface {
-                    val navController = rememberNavController()
-                    NavGraph(navController = navController)
-                }
-            }
+            EquiContiRoot()
+        }
+    }
+}
+
+@Composable
+private fun EquiContiRoot() {
+    val navController = rememberNavController()
+    EquiContiTheme {
+        Surface {
+            AppNavGraph(navController = navController)
         }
     }
 }
