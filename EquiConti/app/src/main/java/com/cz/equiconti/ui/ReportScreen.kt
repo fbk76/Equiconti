c0e9692ee63759533a802a0feb6a6842c7236b36
fbk76 <fadilalb@yahoo.it>
2025-08-11 09:55:30 +0200

    Update ReportScreen.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/ui/ReportScreen.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/ui/ReportScreen.kt
@@ -6,8 +6,8 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import androidx.compose.ui.Modifier
 
 @Composable
 fun ReportScreen(nav: NavController, ownerId: Long) {
