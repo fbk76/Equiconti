bc4f4e68059b976b33f0376fff58f82bfadd99e7
fbk76 <fadilalb@yahoo.it>
2025-08-12 00:49:07 +0200

    Update NavGraph.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/ui/NavGraph.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/ui/NavGraph.kt
@@ -1,19 +1,16 @@
 package com.cz.equiconti.ui
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavType
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.cz.equiconti.data.Owner
 import com.cz.equiconti.ui.owner.AddHorseScreen
 import com.cz.equiconti.ui.owner.AddOwnerScreen
 import com.cz.equiconti.ui.owner.OwnerDetailScreen
 import com.cz.equiconti.ui.owner.OwnersScreen
 import com.cz.equiconti.ui.txn.TxnScreen
-import com.cz.equiconti.ui.owner.OwnersViewModel
 
 @Composable
 fun NavGraph(navController: NavHostController) {
@@ -21,27 +18,19 @@ fun NavGraph(navController: NavHostController) {
         navController = navController,
         startDestination = "owners"
     ) {
-        // Lista proprietari
         composable("owners") {
-            // Se la tua OwnersScreen gestisce internamente i click,
-            // è sufficiente passare il NavController.
-            OwnersScreen(nav = navController)
-        }
-
-        // Aggiunta proprietario
-        composable("addOwner") {
-            val vm: OwnersViewModel = hiltViewModel()
-            AddOwnerScreen(
-                nav = navController,
-                onSave = { owner: Owner ->
-                    vm.save(owner)
-                    // Torna alla lista
-                    navController.popBackStack()
-                }
+            OwnersScreen(
+                nav = navController
             )
         }
 
-        // Dettaglio proprietario
+        composable("addOwner") {
+            AddOwnerScreen(
+                nav = navController,
+                onSave = { /* il salvataggio reale lo gestirai nel ViewModel */ }
+            )
+        }
+
         composable(
             route = "owner/{ownerId}",
             arguments = listOf(navArgument("ownerId") { type = NavType.LongType })
@@ -53,7 +42,6 @@ fun NavGraph(navController: NavHostController) {
             )
         }
 
-        // Aggiunta cavallo
         composable(
             route = "owner/{ownerId}/addHorse",
             arguments = listOf(navArgument("ownerId") { type = NavType.LongType })
@@ -65,7 +53,6 @@ fun NavGraph(navController: NavHostController) {
             )
         }
 
-        // Movimenti
         composable(
             route = "owner/{ownerId}/txns",
             arguments = listOf(navArgument("ownerId") { type = NavType.LongType })
