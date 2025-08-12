74c57dff99f14227bc5271f71ae93dc8f1d30d0d
fbk76 <fadilalb@yahoo.it>
2025-08-13 01:04:51 +0200

    Update HorseDao.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/data/HorseDao.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/data/HorseDao.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface HorseDao {
 
-    // Stesso nome usato in Repo
     @Query("SELECT * FROM Horse WHERE ownerId = :ownerId ORDER BY name")
     fun observeHorses(ownerId: Long): Flow<List<Horse>>
 
@@ -18,7 +17,4 @@ interface HorseDao {
 
     @Delete
     suspend fun delete(horse: Horse)
-
-    @Query("SELECT * FROM Horse WHERE id = :id LIMIT 1")
-    suspend fun getById(id: Long): Horse?
 }
