f48a6ace6b4eedb52e248f4800f757facae6dc34
fbk76 <fadilalb@yahoo.it>
2025-08-12 12:51:45 +0200

    Update Repo.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/data/Repo.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/data/Repo.kt
@@ -1,20 +1,29 @@
 package com.cz.equiconti.data
 
+import android.content.Context
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 
 class Repo(private val db: EquiDb) {
-    // --- OWNER ---
+
+    companion object {
+        fun from(context: Context) = Repo(EquiDb.get(context))
+    }
+
+    /* =============== OWNER =============== */
+
     fun observeOwners(): Flow<List<Owner>> = db.ownerDao().observeAll()
 
-    suspend fun listOwnersOnce(): List<Owner> =
-        db.ownerDao().observeAll().first()
+    suspend fun listOwnersOnce(): List<Owner> = db.ownerDao().observeAll().first()
+
+    suspend fun getOwnerById(id: Long): Owner? = db.ownerDao().getOwnerById(id)
 
     suspend fun upsertOwner(owner: Owner): Long {
         return if (owner.id == 0L) {
             db.ownerDao().insert(owner)
         } else {
-            db.ownerDao().update(owner); owner.id
+            db.ownerDao().update(owner)
+            owner.id
         }
     }
 
