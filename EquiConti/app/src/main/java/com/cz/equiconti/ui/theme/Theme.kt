5336a1fbfd83adf2290257fd891e51500da7bf48
fbk76 <fadilalb@yahoo.it>
2025-08-12 08:24:21 +0200

    Create Theme.kt

---
+++ EquiConti/app/src/main/java/com/cz/equiconti/ui/theme/Theme.kt
@@ -0,0 +1,16 @@
+package com.cz.equiconti.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColors = lightColorScheme()
+
+@Composable
+fun EquicontiTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = LightColors,
+        typography = MaterialTheme.typography,
+        content = content
+    )
+}
