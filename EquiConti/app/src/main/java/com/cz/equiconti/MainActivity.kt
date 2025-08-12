f556a04ac04ef416213a01ed5ac50de80ba3c230
fbk76 <fadilalb@yahoo.it>
2025-08-12 13:04:17 +0200

    Update MainActivity.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/MainActivity.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/MainActivity.kt
@@ -3,18 +3,21 @@ package com.cz.equiconti
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.cz.equiconti.ui.navigation.AppNavGraph
 import dagger.hilt.android.AndroidEntryPoint
-import com.cz.equiconti.ui.AppNavGraph
-import com.cz.equiconti.ui.theme.EquicontiTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // ATTENZIONE: il nome del tema deve esistere in ui/theme
-            EquicontiTheme {
-                AppNavGraph()
+            // Uso direttamente MaterialTheme per evitare riferimenti a temi custom mancanti
+            MaterialTheme {
+                Surface {
+                    AppNavGraph()
+                }
             }
         }
     }
