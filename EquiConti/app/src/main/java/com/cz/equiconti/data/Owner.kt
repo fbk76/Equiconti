69e49574b9fa08525f56f5f0bab254ac91a30970
fbk76 <fadilalb@yahoo.it>
2025-08-13 13:14:25 +0200

    Update Owner.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/data/Owner.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/data/Owner.kt
@@ -3,10 +3,6 @@ package com.cz.equiconti.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-/**
- * Entità proprietario.
- * PK = id (autoGenerate).
- */
 @Entity(tableName = "Owner")
 data class Owner(
     @PrimaryKey(autoGenerate = true) val id: Long = 0L,
