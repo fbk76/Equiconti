5f52b9aa36d6111482879375b44648a8aa5c5a08
fbk76 <fadilalb@yahoo.it>
2025-08-14 11:50:12 +0200

    Update OwnerDao.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/data/OwnerDao.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/data/OwnerDao.kt
@@ -5,13 +5,14 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface OwnerDao {
+
     @Query("SELECT * FROM Owner ORDER BY lastName, firstName")
     fun observeAll(): Flow<List<Owner>>
 
-    @Query("SELECT * FROM Owner WHERE id = :id LIMIT 1")
+    @Query("SELECT * FROM Owner WHERE id = :id")
     suspend fun getById(id: Long): Owner?
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(owner: Owner): Long
 
     @Update
