f136be6b7a0a0c1675cd88cb862dcbea954602c0
fbk76 <fadilalb@yahoo.it>
2025-08-14 18:13:51 +0200

    Update MainActivity.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/MainActivity.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.cz.equiconti.ui.AppNavGraph
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { AppNavGraph() }
+        setContent {
+            AppNavGraph()   // <-- deve esistere in ui/NavGraph.kt con lo stesso nome
+        }
     }
 }
