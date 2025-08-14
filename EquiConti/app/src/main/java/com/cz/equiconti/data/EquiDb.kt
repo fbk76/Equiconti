e93dd4933de2dbc9e8e0e38d33a8c14c7a7d73f0
fbk76 <fadilalb@yahoo.it>
2025-08-14 12:09:55 +0200

    Update EquiDb.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/data/EquiDb.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/data/EquiDb.kt
@@ -7,27 +7,27 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [Owner::class, Horse::class, Txn::class],
-    version = 2,
+    version = 1,
     exportSchema = false
 )
 abstract class EquiDb : RoomDatabase() {
+
     abstract fun ownerDao(): OwnerDao
     abstract fun horseDao(): HorseDao
     abstract fun txnDao(): TxnDao
 
     companion object {
-        @Volatile private var INSTANCE: EquiDb? = null
+        @Volatile
+        private var INSTANCE: EquiDb? = null
 
-        fun get(context: Context): EquiDb =
-            INSTANCE ?: synchronized(this) {
+        fun get(context: Context): EquiDb {
+            return INSTANCE ?: synchronized(this) {
                 INSTANCE ?: Room.databaseBuilder(
                     context.applicationContext,
                     EquiDb::class.java,
-                    "equiconti.db"
-                )
-                .fallbackToDestructiveMigration()
-                .build()
-                .also { INSTANCE = it }
+                    "equidb"
+                ).build().also { INSTANCE = it }
             }
+        }
     }
 }
