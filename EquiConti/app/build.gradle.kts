441a204367396699cfe72b089f51327b35c76e9d
fbk76 <fadilalb@yahoo.it>
2025-08-16 01:07:59 +0200

    Update build.gradle.kts

--- EquiConti/app/build.gradle.kts
+++ EquiConti/app/build.gradle.kts
@@ -48,7 +48,15 @@ android {
 }
 
 dependencies {
-    // Kotlin / AndroidX di base
+  
+// Jetpack Navigation per Compose
+implementation("androidx.navigation:navigation-compose:2.7.7")
+
+// Hilt helpers per Compose (fornisce hiltViewModel())
+implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+
+// Kotlin / AndroidX di base
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
