9c67050bb9ddc0db41ef71cae2864a290a5bbfc8
fbk76 <fadilalb@yahoo.it>
2025-08-16 00:50:24 +0200

    Update OwnerDao.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/data/OwnerDao.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/data/OwnerDao.kt
@@ -2,17 +2,15 @@ package com.cz.equiconti.data
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface OwnerDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(owner: Owner): Long
+
     @Query("SELECT * FROM owners ORDER BY name")
-    fun getOwners(): Flow<List<Owner>>
-
-    @Insert
-    suspend fun insert(owner: Owner): Long
-
-    @Query("SELECT * FROM owners WHERE id = :id")
-    fun getOwner(id: Long): Flow<Owner?>
+    fun all(): Flow<List<Owner>>
 }
