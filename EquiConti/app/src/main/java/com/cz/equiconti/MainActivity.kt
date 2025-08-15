2f7ebefc1e9aea4576df02587d625a4539edca10
fbk76 <fadilalb@yahoo.it>
2025-08-15 08:41:57 +0200

    Update MainActivity.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/MainActivity.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
 import androidx.navigation.compose.rememberNavController
-import com.cz.equiconti.ui.NavGraph
+import com.cz.equiconti.ui.NavGraph()
 import com.cz.equiconti.ui.theme.EquicontiTheme
 import dagger.hilt.android.AndroidEntryPoint
 
