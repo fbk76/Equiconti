6d0c6a2a8b0e1eae47c667d2226c040506b5bf18
fbk76 <fadilalb@yahoo.it>
2025-08-16 00:47:52 +0200

    Update build.gradle.kts

--- EquiConti/app/build.gradle.kts
+++ EquiConti/app/build.gradle.kts
@@ -49,6 +49,9 @@ android {
 
 dependencies {
     // Kotlin / AndroidX di base
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
