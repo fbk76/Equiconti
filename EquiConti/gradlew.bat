bff8c9427aa5a639adcb43235781a95571d33207
fbk76 <fadilalb@yahoo.it>
2025-08-15 21:03:28 +0200

    Update gradlew.bat

--- EquiConti/gradlew.bat
+++ EquiConti/gradlew.bat
@@ -1,22 +1,58 @@
+@REM -----------------------------------------------------------------------------
+@REM Gradle startup script for Windows
+@REM -----------------------------------------------------------------------------
+
 @ECHO OFF
-@rem ##########################################################################
-@rem
-@rem  Gradle startup script for Windows
-@rem
-@rem ##########################################################################
 
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
-
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS=
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
-set JAVA_CMD=java
-if defined JAVA_HOME set JAVA_CMD=%JAVA_HOME%\bin\java.exe
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
-"%JAVA_CMD%" %DEFAULT_JVM_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+@rem Find java.exe
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto init
+
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+goto fail
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+
+if exist "%JAVA_EXE%" goto init
+
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+goto fail
+
+:init
+@rem Get command-line arguments, handling Windows variants
+set CMD_LINE_ARGS=%*
+set STARTER_CLASS=org.gradle.wrapper.GradleWrapperMain
+
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% -Dorg.gradle.appname=%APP_BASE_NAME% -classpath "%CLASSPATH%" %STARTER_CLASS% %CMD_LINE_ARGS%
+goto end
+
+:fail
+exit /b 1
+
+:end
