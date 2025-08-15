adad77f32a2d183da2db87128fafbed9b451b863
fbk76 <fadilalb@yahoo.it>
2025-08-15 23:58:22 +0200

    Update build.gradle.kts

--- EquiConti/app/build.gradle.kts
+++ EquiConti/app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
 
     // Material 3
+
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.compose.material3:material3:1.3.0")
 
     // Compose BOM + moduli principali
