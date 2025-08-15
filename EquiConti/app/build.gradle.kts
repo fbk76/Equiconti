a192914d3365bafd7f427304ce7d05f18c5dfd64
fbk76 <fadilalb@yahoo.it>
2025-08-15 21:37:16 +0200

    Update build.gradle.kts

--- EquiConti/app/build.gradle.kts
+++ EquiConti/app/build.gradle.kts
@@ -1,6 +1,7 @@
+// EquiConti/build.gradle.kts
 plugins {
-    // 🔧 Dichiariamo i plugin con versione a livello di root
+    // Versioni dei plugin definite a livello di root
     id("com.android.application") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false // rimuovi se non usi Hilt
 }
