df654ff2420ab24e94a6ed9cf0b16a1c82f93abb
fbk76 <fadilalb@yahoo.it>
2025-08-15 23:22:37 +0200

    Update build.gradle.kts

--- EquiConti/app/build.gradle.kts
+++ EquiConti/app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.compose.ui:ui")
