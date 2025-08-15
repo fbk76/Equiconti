0c6bb363404e42d8c96e21a66475e277fc030391
fbk76 <fadilalb@yahoo.it>
2025-08-16 01:39:33 +0200

    Update Txn.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/data/Txn.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/data/Txn.kt
@@ -5,7 +5,9 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "txns")
 data class Txn(
-    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+
     val horseId: Long,
     val amountCents: Long,
     val note: String? = null,
