57da4786b72f5920add97f6d29b952b36aa0a2ee
fbk76 <fadilalb@yahoo.it>
2025-08-16 00:26:29 +0200

    Update build.gradle.kts

--- EquiConti/build.gradle.kts
+++ EquiConti/build.gradle.kts
@@ -1,10 +1,14 @@
-// build.gradle.kts (ROOT)
 plugins {
+    // Android Gradle Plugin
     id("com.android.application") version "8.4.2" apply false
+
+    // Kotlin 2.0.20 allineato ovunque
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    // NECESSARIO con Kotlin 2.x + Compose
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
 
-    // Se usi Hilt, lascia, altrimenti puoi rimuoverlo
+    // KSP per Room (versione accoppiata a Kotlin)
+    id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false
+
+    // Hilt
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
