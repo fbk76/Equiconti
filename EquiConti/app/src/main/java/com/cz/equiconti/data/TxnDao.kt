eaff00c32ff7982ba713d718736381e5fc633fd5
fbk76 <fadilalb@yahoo.it>
2025-08-16 02:01:17 +0200

    Update TxnDao.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/data/TxnDao.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/data/TxnDao.kt
@@ -1,11 +1,30 @@
 package com.cz.equiconti.data
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Upsert
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TxnDao {
+
+    // READ
     @Query("SELECT * FROM txns WHERE horseId = :horseId ORDER BY timestamp DESC")
     fun getTxns(horseId: Long): Flow<List<Txn>>
+
+    @Query("SELECT * FROM txns WHERE id = :id")
+    suspend fun getById(id: Long): Txn?
+
+    // WRITE
+    @Upsert
+    suspend fun upsert(txn: Txn)
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insert(txn: Txn): Long
+
+    @Delete
+    suspend fun delete(txn: Txn)
 }
