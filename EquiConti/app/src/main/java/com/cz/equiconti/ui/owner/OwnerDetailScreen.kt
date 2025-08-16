8d54c7b0b75e93d1dfb61871949c8586c4ce740b
fbk76 <fadilalb@yahoo.it>
2025-08-16 09:29:02 +0200

    Update OwnerDetailScreen.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/ui/owner/OwnerDetailScreen.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/ui/owner/OwnerDetailScreen.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ReceiptLong
+import androidx.compose.material.icons.filled.Receipt
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
