49316f7b00864c63a41d965b076f983a35cbe61f
fbk76 <fadilalb@yahoo.it>
2025-08-16 02:00:45 +0200

    Update HorseDao.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/data/HorseDao.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/data/HorseDao.kt
@@ -1,11 +1,30 @@
 package com.cz.equiconti.data
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Upsert
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface HorseDao {
+
+    // READ
     @Query("SELECT * FROM horses WHERE ownerId = :ownerId ORDER BY name")
     fun getHorses(ownerId: Long): Flow<List<Horse>>
+
+    @Query("SELECT * FROM horses WHERE id = :id")
+    suspend fun getById(id: Long): Horse?
+
+    // WRITE
+    @Upsert
+    suspend fun upsert(horse: Horse)
+
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun insert(horse: Horse): Long
+
+    @Delete
+    suspend fun delete(horse: Horse)
 }
