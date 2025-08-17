7dfbe98b8b4b60138d5900247f4bf2db323e0a05
fbk76 <fadilalb@yahoo.it>
2025-08-18 00:03:49 +0200

    Update MainActivity.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/MainActivity.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/MainActivity.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
-import com.cz.equiconti.ui.home.HomeScaffold
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            HomeScaffold()
-        }
+        setContent { EquiContiApp() }
     }
 }
