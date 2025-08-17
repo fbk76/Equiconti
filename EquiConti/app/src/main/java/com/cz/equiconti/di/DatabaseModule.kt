a1a1711142a9e1d94b134cea6b5d854a045913ba
fbk76 <fadilalb@yahoo.it>
2025-08-18 00:06:09 +0200

    Update DatabaseModule.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/di/DatabaseModule.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/di/DatabaseModule.kt
@@ -1,11 +1,8 @@
 package com.cz.equiconti.di
 
 import android.content.Context
-import androidx.room.Room
 import com.cz.equiconti.data.EquiDb
-import com.cz.equiconti.data.OwnerDao
-import com.cz.equiconti.data.HorseDao
-import com.cz.equiconti.data.TxnDao
+import com.cz.equiconti.data.Repo
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,25 +13,8 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
-
-    @Provides
-    @Singleton
-    fun provideDb(@ApplicationContext context: Context): EquiDb =
-        Room.databaseBuilder(
-            context,
-            EquiDb::class.java,
-            "equi.db"
-        )
-            // opzionale: cancella e ricrea se cambi schema senza migrazioni
-            // .fallbackToDestructiveMigration()
-            .build()
-
-    @Provides
-    fun provideOwnerDao(db: EquiDb): OwnerDao = db.ownerDao()
-
-    @Provides
-    fun provideHorseDao(db: EquiDb): HorseDao = db.horseDao()
-
-    @Provides
-    fun provideTxnDao(db: EquiDb): TxnDao = db.txnDao()
+    @Provides @Singleton
+    fun provideDb(@ApplicationContext context: Context): EquiDb = EquiDb.get(context)
+    @Provides @Singleton
+    fun provideRepo(db: EquiDb): Repo = Repo(db)
 }
