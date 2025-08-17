feba0b20e13068199855d4a65fc672275b61e9f3
fbk76 <fadilalb@yahoo.it>
2025-08-17 12:03:32 +0200

    Update build.gradle.kts

--- EquiConti/build.gradle.kts
+++ EquiConti/build.gradle.kts
@@ -1,11 +1,7 @@
 plugins {
     id("com.android.application") version "8.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.24" apply false
-
-    // KSP allineato alla versione Kotlin
+    // rimosso: id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
-
-    // Hilt
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
