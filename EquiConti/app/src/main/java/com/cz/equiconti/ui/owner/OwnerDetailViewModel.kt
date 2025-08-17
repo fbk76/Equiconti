301ceadc33939061c1ae033b9ecb8722b628f243
fbk76 <fadilalb@yahoo.it>
2025-08-17 23:32:41 +0200

    Update OwnerDetailViewModel.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/ui/owner/OwnerDetailViewModel.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/ui/owner/OwnerDetailViewModel.kt
@@ -1,19 +1,31 @@
 package com.cz.equiconti.ui.owner
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.cz.equiconti.data.Horse
 import com.cz.equiconti.data.Owner
 import com.cz.equiconti.data.Repo
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.stateIn
 
 @HiltViewModel
 class OwnerDetailViewModel @Inject constructor(
-    private val repo: Repo
+    repo: Repo,
+    savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
-    fun owner(ownerId: Long): Flow<Owner?> = repo.getOwner(ownerId)
+    val ownerId: Long = checkNotNull(savedStateHandle.get<Long>("ownerId"))
 
-    fun horses(ownerId: Long): Flow<List<Horse>> = repo.getHorses(ownerId)
+    val owner: StateFlow<Owner?> =
+        repo.getOwner(ownerId)
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000), null)
+
+    val horses: StateFlow<List<Horse>> =
+        repo.getHorses(ownerId)
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000), emptyList())
 }
