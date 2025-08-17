e4a639de1c3c376bc64a2cad8247d62ca6f278b8
fbk76 <fadilalb@yahoo.it>
2025-08-17 11:52:56 +0200

    Update build.gradle.kts

--- EquiConti/build.gradle.kts
+++ EquiConti/build.gradle.kts
@@ -1,13 +1,10 @@
 plugins {
-    // Android Gradle Plugin
     id("com.android.application") version "8.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "1.9.24" apply false
 
-    // Kotlin 2.0.20 allineato ovunque
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
-
-    // KSP per Room (versione accoppiata a Kotlin)
-    id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false
+    // KSP allineato alla versione Kotlin
+    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
 
     // Hilt
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
