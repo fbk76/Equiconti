a60e710a47b2f7fe2fc793944f2e7c3c25dc33cb
fbk76 <fadilalb@yahoo.it>
2025-08-18 00:08:37 +0200

    Update OwnerDao.kt

--- EquiConti/app/src/main/java/com/cz/equiconti/data/OwnerDao.kt
+++ EquiConti/app/src/main/java/com/cz/equiconti/data/OwnerDao.kt
@@ -1,23 +1,13 @@
 package com.cz.equiconti.data
-
 import androidx.room.Dao
-import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Upsert
 import kotlinx.coroutines.flow.Flow
-
 @Dao
 interface OwnerDao {
-
-    @Query("SELECT * FROM owners ORDER BY id DESC")
-    fun getAll(): Flow<List<Owner>>
-
-    @Query("SELECT * FROM owners WHERE id = :ownerId LIMIT 1")
-    fun getById(ownerId: Long): Flow<Owner?>
-
-    @Upsert
-    suspend fun upsert(owner: Owner)
-
-    @Delete
-    suspend fun delete(owner: Owner)
+    @Query("SELECT * FROM owners ORDER BY name")
+    fun getOwners(): Flow<List<Owner>>
+    @Insert suspend fun insert(owner: Owner): Long
+    @Query("SELECT * FROM owners WHERE id = :id")
+    fun getOwner(id: Long): Flow<Owner?>
 }
